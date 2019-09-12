55d9aab5c9ef51efeedaa65ef857b9a0b2bf4a41
Heena <heenaj198@gmail.com>
2019-09-12 09:51:57 +0530

    added components

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Item from './Item.js'
+import New from './new.js'
+import Game from './Game.js'
+function App() {
+  return (
+    <Game />
+  );
+}
+
+export default App;
