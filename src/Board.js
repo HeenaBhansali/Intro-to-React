45a66b4718b2d8cd1625e9a4c178f1a5038afd93
Heena <heenaj198@gmail.com>
2019-09-18 16:44:30 +0530

    Refactored Code with State Hooks

--- src/Board.js
+++ src/Board.js
@@ -1,51 +1,28 @@
-
 import React from 'react'
 import Square from './Square.js'
-import './index.css'
-class Board extends React.Component {
-  // handleClick(i){
-  //   const history = this.state.history
-  //   const current = history[history.length - 1]
-  //   const squares = current.squares.slice()
-  //    if (calculateWinner(squares) || squares[i]) {
-  //     return;
-  //   }
-  //   squares[i] = this.state.xIsNext ? 'X' : 'O'
-  //   this.setState({
-  //     history: history.concat([{
-  //       squares: squares,
-  //     }]),
-  //     xIsNext : !this.state.xIsNext,
-  //   })
-  //  }    
-  renderSquare (i){
-  return (
-     <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />)
-}  
-render(){
-     return (
-      <div>
-        <div className = 'board-game'>
-             {this.renderSquare(0)}
-             {this.renderSquare(1)}
-             {this.renderSquare(2)}
-        </div>
-         <div className = 'board-game'>
-             {this.renderSquare(3)}
-             {this.renderSquare(4)}
-             {this.renderSquare(5)}
-        </div>
-         <div className = 'board-game'>
-             {this.renderSquare(6)}
-             {this.renderSquare(7)}
-             {this.renderSquare(8)}
-        </div>
 
+const Board = ({ squares, onClick }) => {
+  const renderSquare = i => <Square value={squares[i]} onClick={() => onClick(i)} />
+
+  return (
+    <div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
-)}
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  )
 }
 
 export default Board
